f4b7452436e611b3e2c95ecd6bbe604811896da2
Fachrezandi20 <andi.muh.ft20@mail.umy.ac.id>
2023-04-01 22:02:49 +0700

    menambahkan konten text dan image pada welcome

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -7,6 +7,42 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: const [
+          SizedBox(
+            height: 90,
+          ),
+          Text(
+            "Pokedex.",
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+              color: Colors.amber,
+            ),
+          ),
+          SizedBox(
+            height: 59,
+          ),
+          Text(
+            "Welcome to Pokedex",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: 110,
+          ),
+          Image(
+            image: AssetImage("image/pokeball2.png"),
+            height: 144,
+            width: double.infinity,
+            alignment: Alignment.bottomCenter,
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
