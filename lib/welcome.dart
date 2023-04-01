7e0378d38e46a95f788fe7e5b415e4ae36d4f5b3
Fachrezandi20 <andi.muh.ft20@mail.umy.ac.id>
2023-04-01 21:55:11 +0700

    add image

---
+++ lib/welcome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
